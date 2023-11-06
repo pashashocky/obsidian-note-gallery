b62e99c46f4cdf34983eab626e295f2416320244
Pash Shocky <pashashocky@gmail.com>
2023-11-06 18:21:15 +0000

    feat(code-block): enable clicking on images

--- src/code-block/index.tsx
+++ src/code-block/index.tsx
@@ -10,6 +10,7 @@ import React, {
   useRef,
   PropsWithChildren,
   StrictMode,
+  MouseEvent,
 } from "react";
 import { createRoot, Root } from "react-dom/client";
 
@@ -38,20 +39,17 @@ type CardProps = CardPropsI & React.HTMLAttributes<HTMLDivElement> & WithKeyProp
 const Card = (props: PropsWithChildren<CardProps>) => {
   const { file, ...rest } = props;
   const { app, sourcePath } = useAppMount();
+  const handleClick = (event: MouseEvent): void => {
+    const newLeaf =
+      event.altKey && (event.ctrlKey || event.metaKey)
+        ? "split"
+        : event.ctrlKey || event.metaKey
+        ? "tab"
+        : false;
+    app.workspace.openLinkText(file.path, sourcePath, newLeaf);
+  };
   return (
-    <div
-      {...rest}
-      className="note-card"
-      onClick={event => {
-        const newLeaf =
-          event.altKey && (event.ctrlKey || event.metaKey)
-            ? "split"
-            : event.ctrlKey || event.metaKey
-            ? "tab"
-            : false;
-        app.workspace.openLinkText(file.basename, sourcePath, newLeaf);
-      }}
-    >
+    <div {...rest} className="note-card" onClick={handleClick}>
       {props.children}
     </div>
   );
